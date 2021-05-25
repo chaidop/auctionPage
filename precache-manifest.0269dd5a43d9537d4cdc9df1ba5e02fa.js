eccfcaa1daf9575992ecf648207c8fd76138e13c
root <poulianouxaido@gmail.com>
2021-05-25 12:57:20 +0300

    Updates

---
+++ precache-manifest.0269dd5a43d9537d4cdc9df1ba5e02fa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a26609ebe317cb7a8e03ec1f04d0fd6",
+    "url": "/auctionPage/index.html"
+  },
+  {
+    "revision": "81339819fec99f3769c9",
+    "url": "/auctionPage/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "5ec22681ae39ce6b514e",
+    "url": "/auctionPage/static/js/2.f30aae4f.chunk.js"
+  },
+  {
+    "revision": "81339819fec99f3769c9",
+    "url": "/auctionPage/static/js/main.89502525.chunk.js"
+  },
+  {
+    "revision": "62fb0fa514103a067134",
+    "url": "/auctionPage/static/js/runtime-main.6bdbeed8.js"
+  }
+]);
